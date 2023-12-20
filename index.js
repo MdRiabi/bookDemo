fe862e19dabadf6388b483aec50fa2e1b07a3200
MdRiabi <mhdriabi2013@gmail.com>
2023-12-20 01:02:33 +0100

    firebase initial config

---
+++ index.js
@@ -0,0 +1,13 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+//"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkWtS_mt-o0as-MbUT8eG8vR_ks3O8QCI",
+    authDomain: "bookdemo-327a8.firebaseapp.com",
+    projectId: "bookdemo-327a8",
+    storageBucket: "bookdemo-327a8.appspot.com",
+    messagingSenderId: "370023910423",
+    appId: "1:370023910423:web:7cf13bb54cfe54b7ebe052"
+  };
+
+  const app = initializeApp(firebaseConfig);

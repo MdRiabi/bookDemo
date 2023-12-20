f555f7c06e8ab3b3531b3654eb38280e483c1dd8
MdRiabi <mhdriabi2013@gmail.com>
2023-12-20 02:19:27 +0100

    showing the inputs in the html page using two methods

--- index.js
+++ index.js
@@ -30,5 +30,40 @@
     })
     .then(() =>{
         addForm.reset();
+        showCard();
     })
   });
+
+  //  showing the inserted card in the html page 
+
+  const cards = document.querySelector(".cards");
+  function showCard(){
+    cards.innerHTML = "";
+    getDocs(colRef)
+     .then(data =>{
+        data.docs.forEach(document =>{
+            cards.innerHTML += generateTemplate(document.data(), document.id);
+        })
+     })
+     .catch(error =>{
+        console.log(`Error getting documents ${error}`);
+     })
+  }
+
+  showCard();
+
+
+function generateTemplate(response, id){
+
+    return `<div class="card">
+                <p class="title">${response.title}</p>
+                <div class="sub-information">
+                    <p>
+                        <span class="category ${response.category}">${response.category[0].toUpperCase()}${response.category.slice(1)}</span>
+                    </p>
+                    <a href="${response.link}" target="_blank"><i class="bi bi-box-arrow-up-right website"></i></a>
+                    <a href="https://www.google.com/search?q=${response.title}" target="_blank"><i class="bi bi-google search"></i></a>
+                    <span><i class="bi bi-trash delete" data-id="${id}"></i></span>
+                </div>
+            </div>`;
+}
